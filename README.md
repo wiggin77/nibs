2e1a1e5de2e21daa75ee6069a47c8983514fc9e4
wiggin77 <wiggin77@warpmail.net>
2018-01-21 19:17:49 -0500

    wip

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Build Status](https://travis-ci.org/wiggin77/nibs.svg?branch=master)](https://travis-ci.org/wiggin77/nibs)
 
 # nibs
-Read a stream of bytes in nibbles of bits anywhere from 1 bit to 64 bits at a time.
+Nibs is a Go package for reading streams of bytes in nibbles. Each nibble can be a number of bits in the range 1 bit to 64 bits inclusive.
 
 ## Usage
 See [godoc](https://godoc.org/github.com/wiggin77/nibs).
--- nibs.go
+++ nibs.go
@@ -1,42 +1,106 @@
 package nibs
 
 import (
-	"bufio"
 	"errors"
 	"io"
 )
 
 var (
-	// ErrNibbleSize is error used when an invalid nibble size is passed to a read method.
+	// ErrNibbleSize is the error used when an invalid nibble size is passed to a read method.
 	ErrNibbleSize = errors.New("invalid nibble size (must be 1-64 bits inclusive)")
+
+	// ErrNotEOF is the error used when trying to check how many bits are remaining but
+	// not yet at end of file.
+	ErrNotEOF = errors.New("not at EOF")
 )
 
 // Nibs reads a stream of bytes in nibbles of 1 bit to 64 bits.
 type Nibs struct {
-	r   io.ByteReader
-	b   byte
-	pos byte
+	reader   io.Reader
+	curr     [8]byte
+	usedCurr int // how many bytes in `curr` were read
+	next     [8]byte
+	usedNext int  // how many bytes in `next` were read
+	pos      int  // bit position of last nibble (1-64)
+	eof      bool // true if EOF is reached for `r` (`next` is empty)
 }
 
 // New returns a new Nibs which reads from the specified io.Reader.
 func New(r io.Reader) *Nibs {
-	return &Nibs{r: bufio.NewReader(r)}
+	return &Nibs{reader: r}
+}
+
+// BitsRemaining returns the number of bits that can be read once EOF is reached.
+// Reading more than this number causes `Nibble` to return io.ErrUnexpectedEOF.
+// If EOF is not yet reached then nibs.ErrNotEOF is returned.
+// If error is nil and zero is returned then all the bits in the stream have been read.
+func (n *Nibs) BitsRemaining() (int, error) {
+	if !n.eof {
+		return 0, ErrNotEOF
+	}
+	return (n.usedCurr * 8) - n.pos, nil
 }
 
 // Nibble reads `bits` number of bits from the byte stream and returns the
-// value as a int64.
+// value as a uint64.
 //
 // `bits` must be in the range 1 to 64 inclusive, otherwise
-// nibs.ErrNibbleSize is returned. A value of 0 is always returned for any
-// non-nil error.
+// nibs.ErrNibbleSize is returned.
 //
-//
-func (n *Nibs) Nibble(int bits) (int64, error) {
+// io.EOF is returned when exactly all the bits in the stream have been read.
+// io.ErrUnexpectedEOF is returned when trying to read more bits than are left
+// in the stream.  A value of 0 is always returned for any non-nil error.
+func (n *Nibs) Nibble(bits int) (uint64, error) {
+	if bits < 1 || bits > 64 {
+		return 0, ErrNibbleSize
+	}
+	// check if all bits already read or trying to read more bits than available
+	if n.eof {
+		remaining := (n.usedCurr * 8) - n.pos
+		if remaining == 0 {
+			return 0, io.EOF
+		}
+		if bits > remaining {
+			return 0, io.ErrUnexpectedEOF
+		}
+	}
 
+	var ret uint64
+	for i := 0; i < bits; i++ {
+		bit, err := n.nextBit()
+		if err != nil {
+			return 0, err
+		}
+		ret = ret << 1
+		ret = ret & uint64(bit)
+	}
+	return ret, nil
 }
 
-// Remainder returns the value of any bits leftover in the current byte.
-// Use this once io.EOF is reached to check if any bits were left over.
-func (n *Nibs) Remainder() byte {
+func (n *Nibs) nextBit() (byte, error) {
+	n.pos++
+	// check if we need to read more bytes
+	if n.pos > n.usedCurr*8 {
+		if n.eof {
+			return 0, io.EOF
+		}
+		n.curr = n.next
+		n.usedCurr = n.usedNext
+		n.pos = 0
+		var err error
+		if n.usedNext, err = n.reader.Read(n.next[:]); err != nil {
+			if err == io.EOF {
+				n.eof = true
+			} else {
+				return 0, err
+			}
+		}
+	}
 
+	// get the correct byte based on pos
+	b := n.curr[n.pos % 8]
+	// shift the bit we want to the rightmost
+	b >> ()
+	// return 1 or 0
+	return b & 1, nil
 }
